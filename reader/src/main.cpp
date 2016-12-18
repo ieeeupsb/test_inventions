8eb3ef61f8f1874fb18c14cbaf42fa1ba5ac171e
Gonçalo Camelo Neves Pereira <goncalo_pereira@outlook.pt>
2016-12-18 19:30:43 +0000

    Wifi Enable

--- reader/src/main.cpp
+++ reader/src/main.cpp
@@ -1,37 +1,33 @@
-#include <Arduino.h>
 #include <SPI.h>
 #include <MFRC522.h>
 #include <sha256.h>
+#include <ESP8266WiFi.h>
 
-#define RST_PIN         9          // Configurable, see typical pin layout above
-#define SS_1_PIN        10         // Configurable, take a unused pin, only HIGH/LOW required, must be diffrent to SS 2
-#define SS_2_PIN        8          // Configurable, take a unused pin, only HIGH/LOW required, must be diffrent to SS 1
+#define RST_PIN 9   // Configurable, see typical pin layout above
+#define SS_1_PIN 10 // Configurable, take a unused pin, only HIGH/LOW required, must be diffrent to SS 2
+#define SS_2_PIN 8  // Configurable, take a unused pin, only HIGH/LOW required, must be diffrent to SS 1
 
 #define NR_OF_READERS   2
+#define wifiSSID "ssid"
+#define wifiPassword "password"
 
 byte ssPins[] = {SS_1_PIN, SS_2_PIN};
 
 MFRC522 mfrc522[NR_OF_READERS];   // Create MFRC522 instance.
 
-/**
- * Helper routine to dump a byte array as hex values to Serial.
- */
 void dump_byte_array(byte *buffer, byte bufferSize) {
   for (byte i = 0; i < bufferSize; i++) {
     Serial.print(buffer[i] < 0x10 ? " 0" : " ");
     Serial.print(buffer[i], HEX);
+void setup()
+{
+  WiFi.begin(wifiSSID, wifiPassword);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(100);
   }
-}
 
-/**
- * Initialize.
- */
-void setup() {
-
-  Serial.begin(9600); // Initialize serial communications with the PC
-  while (!Serial);    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
-
-  SPI.begin();        // Init SPI bus
+  SPI.begin(); // Init SPI bus
 
   for (uint8_t reader = 0; reader < NR_OF_READERS; reader++) {
     mfrc522[reader].PCD_Init(ssPins[reader], RST_PIN); // Init each MFRC522 card
@@ -42,9 +38,6 @@ void setup() {
   }
 }
 
-/**
- * Main loop.
- */
 void loop() {
 
   for (uint8_t reader = 0; reader < NR_OF_READERS; reader++) {
