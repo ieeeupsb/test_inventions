932235ce39aa4204f87946b5157827f1e659037d
Sleepy105 <lm.sousa@fe.up.pt>
2016-12-19 18:56:06 +0000

    Now hashing uid before sending to server for authentication.

--- reader/src/main.cpp
+++ reader/src/main.cpp
@@ -7,6 +7,9 @@
 #define RST_PIN 9   // Configurable, see typical pin layout above
 #define SS_1_PIN 10 // Configurable, take a unused pin, only HIGH/LOW required, must be diffrent to SS 2
 #define SS_2_PIN 8  // Configurable, take a unused pin, only HIGH/LOW required, must be diffrent to SS 1
+#define RED_LED_PIN 1
+#define YELLOW_LED_PIN 2
+#define GREEN_LED_PIN 3
 
 #define wifiSSID "ssid"
 #define wifiPassword "password"
@@ -15,8 +18,37 @@ byte ssPins[] = {SS_1_PIN, SS_2_PIN};
 
 MFRC522 mfrc522; // Create MFRC522 instance.
 
+uint8_t hmacKey[]={
+  0x0b,0x0b,0x0b,0x0b,0x0b,0x0b,0x0b,0x0b,0x0b,0x0b,0x0b,0x0b,0x0b,0x0b,0x0b,0x0b,0x0b,0x0b,0x0b,0x0b
+};
+
+#define HMAC_KEY_LENGHT 20
+
+String generateHash(String data, const uint8_t* key, int key_length){
+  Sha256.initHmac(key, key_length); // key, and length of key in bytes
+  Sha256.print(data);
+  return Sha256.resultHmac();
+}
+
+void red(bool state, int pin = RED_LED_PIN){
+  digitalWrite(pin, state);
+}
+void yellow(bool state, int pin = YELLOW_LED_PIN){
+  digitalWrite(pin, state);
+}
+void green(bool state, int pin = GREEN_LED_PIN){
+  digitalWrite(pin, state);
+}
+
 void setup()
 {
+  pinMode(RED_LED_PIN, OUTPUT);
+  pinMode(YELLOW_LED_PIN, OUTPUT);
+  pinMode(GREEN_LED_PIN, OUTPUT);
+  red(true);
+  yellow(true);
+  green(true);
+
   WiFi.begin(wifiSSID, wifiPassword);
   while (WiFi.status() != WL_CONNECTED)
   {
@@ -27,12 +59,18 @@ void setup()
 
   mfrc522.PCD_Init(ssPins[0], RST_PIN); // Init each MFRC522 card
   mfrc522.PCD_DumpVersionToSerial();
+
+  red(false);
+  yellow(false);
+  green(false);
+
 }
 
 void loop()
 {
   if (WiFi.status() == WL_CONNECTED && mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial())
   {
+    yellow(true);
     String uid = "";
     byte *buffer = mfrc522.uid.uidByte;
     byte bufferSize = mfrc522.uid.size;
@@ -45,8 +83,23 @@ void loop()
     http.begin("http://haxor.fe.up.pt:3000/entry");
     http.addHeader("Content-Type", "text/plain");
 
-    int httpCode = http.POST(uid);
-    String payload = http.getString();
+    int httpCode = http.POST(generateHash(uid, hmacKey, HMAC_KEY_LENGTH));
+    String payload_s = http.getString();
+
+    int payload_i = payload_s.toInt();
+
+    if(String(payload_i) == payload_s && !payload_i){ //this insures that the conversion went well, because when it fails it returns 0, which is a valid input.
+      yellow(false);
+      green(true);
+      delay(2000);
+      green(false);
+    }
+    else{
+      yellow(false);
+      red(true);
+      delay(2000);
+      red(false);
+    }
 
     http.end();
     // Halt PICC
