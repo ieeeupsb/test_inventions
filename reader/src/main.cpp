0632508849275dc4765a8838cef8ce43282e64f6
Sleepy105 <lm.sousa@fe.up.pt>
2016-12-20 21:07:22 +0000

    Fixed some of G-Pereira's Spaghetti code. :P

--- reader/src/main.cpp
+++ reader/src/main.cpp
@@ -1,28 +1,38 @@
+#include <Arduino.h>
 #include <SPI.h>
 #include <MFRC522.h>
 #include <sha256.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
-#define RST_PIN 9   // Configurable, see typical pin layout above
-#define SS_1_PIN 10 // Configurable, take a unused pin, only HIGH/LOW required, must be diffrent to SS 2
-#define SS_2_PIN 8  // Configurable, take a unused pin, only HIGH/LOW required, must be diffrent to SS 1
-#define RED_LED_PIN 1
-#define YELLOW_LED_PIN 2
-#define GREEN_LED_PIN 3
+/******************************************* START OF CONFIGURATIONS ******************************************/
 
+//SPI bus pins
+#define RST_PIN         0   //D3
+#define SS_PIN          2   //D4
+
+//LED pins
+#define RED_LED_PIN     16  //D0
+#define YELLOW_LED_PIN  5   //D1
+#define GREEN_LED_PIN   4   //D2
+
+//WiFi credentials
 #define wifiSSID "ssid"
 #define wifiPassword "password"
-
-byte ssPins[] = {SS_1_PIN, SS_2_PIN};
-
-MFRC522 mfrc522; // Create MFRC522 instance.
 
 uint8_t hmacKey[]={
   0x0b,0x0b,0x0b,0x0b,0x0b,0x0b,0x0b,0x0b,0x0b,0x0b,0x0b,0x0b,0x0b,0x0b,0x0b,0x0b,0x0b,0x0b,0x0b,0x0b
 };
 
-#define HMAC_KEY_LENGTH 20
+#define HMAC_KEY_LENGTH 20 //This number must reflect the number of bytes in 'hmacKey'.
+
+/******************************************** END OF CONFIGURATIONS *******************************************/
+/******************* DO NOT CHANGE CODE AFTER THIS POINT UNLESS YOU KNOW WHAT YOU ARE DOING *******************/
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+
+String payload_s = "";
+int payload_i = 0;
 
 char* generateHash(String data, const uint8_t* key, int key_length){
   Sha256.initHmac(key, key_length); // key, and length of key in bytes
@@ -89,7 +99,7 @@ void setup(){
 
   SPI.begin(); //Init SPI bus
 
-  mfrc522.PCD_Init(ssPins[0], RST_PIN); //Init each MFRC522 card
+  mfrc522.PCD_Init(); //Init each MFRC522 card
   mfrc522.PCD_DumpVersionToSerial();
 
   red(false);     //
@@ -99,48 +109,65 @@ void setup(){
 }
 
 void loop(){
-  if (WiFi.status() == WL_CONNECTED && mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()){
-    yellow(true); //Indicate that the reading has started.
 
-    String uid = "";
-    byte *buffer = mfrc522.uid.uidByte;
-    byte bufferSize = mfrc522.uid.size;
-    for (byte i = 0; i < bufferSize; i++){ //Populate the 'uid' string
-      uid += buffer[i] < 0x10 ? " 0" : " ";
-      uid += buffer[i], HEX;
-    }
+  payload_s = "";
+  payload_i = 0;
 
-    HTTPClient http;
-    http.begin("http://haxor.fe.up.pt:3000/entry");
-    http.addHeader("Content-Type", "text/plain");
+  // Look for new cards
+	if(!mfrc522.PICC_IsNewCardPresent())
+		return;
 
-    int httpCode = http.POST(generateHash(uid, hmacKey, HMAC_KEY_LENGTH)); //HTTP POST and http response code to such POST
-    String payload_s = http.getString(); //Get response string to the POST.
+	// Select one of the cards
+	if(!mfrc522.PICC_ReadCardSerial())
+		return;
 
-    int payload_i = payload_s.toInt(); //Converts the receieved string to an integer.
+  yellow(true); //Indicate that the reading has started.
 
-    /*
-     *
-     *Do Something with the http code. Nedd to do some research first.
-     *
-    */
-
-    if(String(payload_i) == payload_s && !payload_i){ //This insures that the conversion went well, because when it fails it returns 0, which is a valid input.
-      yellow(false); //Turn off the yellow LED, because we are about to show a response
-      green(true); //Turn on the green LED to show that the uid has accepted by the server.
-      delay(2000); //Wait two seconds to give time for the user to see the response.
-      green(false); //Turn off the green LED
-    }
-    else{
-      yellow(false); //Turn off the yellow LED, because we are about to show a response
-      red(true); //Turn on the red LED to show that the uid has rejected by the server. The user is either not registered, or the uid has not read correctly.
-      delay(2000); //Wait two seconds to give time for the user to see the response.
-      red(false); //Turn off the red LED
-    }
-
-    http.end();
-
-    mfrc522.PICC_HaltA(); //Halt PICC
-    mfrc522.PCD_StopCrypto1(); //Stop encryption on PCD
+  String uid = "";
+  byte *buffer = mfrc522.uid.uidByte;
+  byte bufferSize = mfrc522.uid.size;
+  for (byte i = 0; i < bufferSize; i++){ //Populate the 'uid' string
+    uid += buffer[i] < 0x10 ? " 0" : " ";
+    uid += buffer[i], HEX;
   }
+
+  HTTPClient http;
+  http.begin("http://haxor.fe.up.pt:3000/entry");
+  http.addHeader("Content-Type", "text/plain");
+
+  int httpCode = http.POST(generateHash(uid, hmacKey, HMAC_KEY_LENGTH)); //HTTP POST and http response code to such POST
+
+  // httpCode will be negative on error
+  if(httpCode > 0){
+    // HTTP header has been send and Server response header has been handled
+    Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+    // file found at server
+    if(httpCode == HTTP_CODE_OK) {
+      payload_s = http.getString(); //Get response string to the POST.
+      payload_i = payload_s.toInt(); //Converts the receieved string to an integer.
+
+      Serial.println(payload_s);
+    }
+  }
+  else
+    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+
+  http.end();
+
+  if(String(payload_i) == payload_s && !payload_i){ //This insures that the conversion went well, because when it fails it returns 0, which is a valid input.
+    yellow(false); //Turn off the yellow LED, because we are about to show a response
+    green(true); //Turn on the green LED to show that the uid has accepted by the server.
+    delay(2000); //Wait two seconds to give time for the user to see the response.
+    green(false); //Turn off the green LED
+  }
+  else{
+    yellow(false); //Turn off the yellow LED, because we are about to show a response
+    red(true); //Turn on the red LED to show that the uid has rejected by the server. The user is either not registered, or the uid has not read correctly.
+    delay(2000); //Wait two seconds to give time for the user to see the response.
+    red(false); //Turn off the red LED
+  }
+
+  mfrc522.PICC_HaltA(); //Halt PICC
+  mfrc522.PCD_StopCrypto1(); //Stop encryption on PCD
 }
