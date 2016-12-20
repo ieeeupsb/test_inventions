47d2f2083a854f1c648c3f833dd1563fd4087337
Gonçalo Camelo Neves Pereira <goncalo_pereira@outlook.pt>
2016-12-20 02:10:35 +0000

    Cleaned dependencies

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 var express = require("express"),
     app = express(),
-    bodyParser = require("body-parser"),
     mongoose = require("mongoose");
 
 var port = 3000;
@@ -8,7 +7,6 @@ var port = 3000;
 var indexRoutes = require("./routes/index");
 
 mongoose.connect("mongodb://localhost/rfid");
-app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
