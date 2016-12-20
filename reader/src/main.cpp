6602fa92dfbf18740b38f456a85b40eba2969cf3
Sleepy105 <lm.sousa@fe.up.pt>
2016-12-20 21:16:19 +0000

    Removed some unnecessary lines of code. Fixed typos. Moved url to configurations

--- reader/src/main.cpp
+++ reader/src/main.cpp
@@ -26,6 +26,8 @@ uint8_t hmacKey[]={
 
 #define HMAC_KEY_LENGTH 20 //This number must reflect the number of bytes in 'hmacKey'.
 
+const char* url = "http://haxor.fe.up.pt:3000/entry";
+
 /******************************************** END OF CONFIGURATIONS *******************************************/
 /******************* DO NOT CHANGE CODE AFTER THIS POINT UNLESS YOU KNOW WHAT YOU ARE DOING *******************/
 
@@ -132,7 +134,7 @@ void loop(){
   }
 
   HTTPClient http;
-  http.begin("http://haxor.fe.up.pt:3000/entry");
+  http.begin(url);
   http.addHeader("Content-Type", "text/plain");
 
   int httpCode = http.POST(generateHash(uid, hmacKey, HMAC_KEY_LENGTH)); //HTTP POST and http response code to such POST
@@ -140,7 +142,7 @@ void loop(){
   // httpCode will be negative on error
   if(httpCode > 0){
     // HTTP header has been send and Server response header has been handled
-    Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+    //Serial.printf("[HTTP] POST... code: %d\n", httpCode);
 
     // file found at server
     if(httpCode == HTTP_CODE_OK) {
@@ -150,8 +152,8 @@ void loop(){
       Serial.println(payload_s);
     }
   }
-  else
-    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  //else
+    //Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
 
   http.end();
 
