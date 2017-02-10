349eb408e73e35133194d99355583d1c80efe2f5
Gonçalo Camelo Neves Pereira <goncalo_pereira@outlook.pt>
2017-02-10 00:00:11 +0000

    Revert "Remove trash"
    This reverts commit ac8e3100a1cb087ba31b29d936428c85761d11dd.

---
+++ reader/cmake-build-debug/CMakeFiles/reader.dir/DependInfo.cmake
@@ -0,0 +1,64 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Goncalo/Desktop/inventions_RFId_CheckIn/reader/src/main.cpp" "C:/Users/Goncalo/Desktop/inventions_RFId_CheckIn/reader/cmake-build-debug/CMakeFiles/reader.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ARDUINO=20300"
+  "ARDUINO_ARCH_ESP8266"
+  "ARDUINO_ESP8266_NODEMCU"
+  "ESP8266"
+  "F_CPU=80000000L"
+  "ICACHE_FLASH"
+  "LWIP_OPEN_SRC"
+  "PLATFORMIO=30201"
+  "__ets__"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/Users/Goncalo/.platformio/packages/framework-arduinoespressif8266/tools/sdk/include"
+  "C:/Users/Goncalo/.platformio/packages/framework-arduinoespressif8266/tools/sdk/lwip/include"
+  "C:/Users/Goncalo/.platformio/packages/framework-arduinoespressif8266/cores/esp8266"
+  "C:/Users/Goncalo/.platformio/packages/framework-arduinoespressif8266/variants/nodemcu"
+  "../src"
+  "C:/Users/Goncalo/.platformio/packages/framework-arduinoespressif8266/libraries/ArduinoOTA"
+  "C:/Users/Goncalo/.platformio/packages/framework-arduinoespressif8266/libraries/DNSServer/src"
+  "C:/Users/Goncalo/.platformio/packages/framework-arduinoespressif8266/libraries/EEPROM"
+  "C:/Users/Goncalo/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266AVRISP/src"
+  "C:/Users/Goncalo/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266HTTPClient/src"
+  "C:/Users/Goncalo/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266HTTPUpdateServer/src"
+  "C:/Users/Goncalo/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266SSDP"
+  "C:/Users/Goncalo/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266WebServer/src"
+  "C:/Users/Goncalo/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266WiFi/src"
+  "C:/Users/Goncalo/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266WiFiMesh/src"
+  "C:/Users/Goncalo/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266httpUpdate/src"
+  "C:/Users/Goncalo/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266mDNS"
+  "C:/Users/Goncalo/.platformio/packages/framework-arduinoespressif8266/libraries/Ethernet/src"
+  "C:/Users/Goncalo/.platformio/packages/framework-arduinoespressif8266/libraries/GDBStub/src"
+  "C:/Users/Goncalo/.platformio/packages/framework-arduinoespressif8266/libraries/Hash/src"
+  "C:/Users/Goncalo/.platformio/packages/framework-arduinoespressif8266/libraries/SD/src"
+  "C:/Users/Goncalo/.platformio/packages/framework-arduinoespressif8266/libraries/SPI"
+  "C:/Users/Goncalo/.platformio/packages/framework-arduinoespressif8266/libraries/Servo/src"
+  "C:/Users/Goncalo/.platformio/packages/framework-arduinoespressif8266/libraries/SoftwareSerial"
+  "C:/Users/Goncalo/.platformio/packages/framework-arduinoespressif8266/libraries/TFT_Touch_Shield_V2"
+  "C:/Users/Goncalo/.platformio/packages/framework-arduinoespressif8266/libraries/Ticker"
+  "C:/Users/Goncalo/.platformio/packages/framework-arduinoespressif8266/libraries/Wire"
+  "C:/Users/Goncalo/.platformio/packages/framework-arduinoespressif8266/libraries/esp8266/src"
+  "C:/Users/Goncalo/.platformio/packages/toolchain-xtensa/xtensa-lx106-elf/include"
+  "C:/Users/Goncalo/.platformio/packages/toolchain-xtensa/lib/gcc/xtensa-lx106-elf/4.8.2/include"
+  "C:/Users/Goncalo/.platformio/packages/toolchain-xtensa/lib/gcc/xtensa-lx106-elf/4.8.2/include-fixed"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
